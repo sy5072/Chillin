6dbb518ceb351878aa7ecbe05c9542eaf1a94913
Nick(Seungyun Kim) <sy5072@naver.com>
2022-05-02 21:39:15 +0900

    Initial Commit

---
+++ chillin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chillin
+//
+//  Created by Seungyun Kim on 2022/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chillin/chillinApp.swift
@@ -0,0 +1,17 @@
+//
+//  chillinApp.swift
+//  chillin
+//
+//  Created by Seungyun Kim on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct chillinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
