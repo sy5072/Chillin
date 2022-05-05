fba8bbba862220268e2766def93b8e855e3e3d4d
Nick(Seungyun Kim) <sy5072@naver.com>
2022-05-05 21:00:32 +0900

    [Feat] Card detail 레이아웃

--- chillin/Screens/Detail/View/DetailView.swift
+++ chillin/Screens/Detail/View/DetailView.swift
@@ -1,20 +1,79 @@
 //
-//  DetailView.swift
+//  CardView.swift
 //  chillin
 //
-//  Created by Seungyun Kim on 2022/05/03.
+//  Created by Seungyun Kim on 2022/05/04.
 //
 
 import SwiftUI
 
 struct DetailView: View {
+    
+    let card: TownCard
+    let cardThumbNailCornerRadius: CGFloat = 10
+    let cardPadding: CGFloat = 8
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            Button(action: {
+                print("add!!")
+            }) {
+                Image(systemName: "chevron.down")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width/16)
+                    .colorMultiply(.gray)
+            }
+            
+            HStack {
+                Text(card.townName)
+                    
+                    .font(.system(size: 16)
+                        .weight(.semibold)
+                    )
+                    .foregroundColor(.lightBlue)
+                    
+                
+                Spacer()
+                
+                Button(action: {
+                    print("add!!")
+                }) {
+                    Image("img_addButton")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width/9)
+                    
+                }
+                
+            }
+            
+            Image(card.image?[0] ?? "img_plzAdd")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(cardThumbNailCornerRadius)
+                .customShadow()
+                .padding(.bottom)
+            
+            Text(card.townGuide)
+                .frame(height: UIScreen.main.bounds.height/2, alignment: .top)
+            
+            
+                
+
+        }
+        .frame(width: UIScreen.main.bounds.width*7/8, height: UIScreen.main.bounds.height*4/5)
     }
+    
+   
 }
 
+
 struct DetailView_Previews: PreviewProvider {
+    static var card = TownCard.sampleData[0]
     static var previews: some View {
-        DetailView()
+        DetailView(card: card)
+        
     }
 }
