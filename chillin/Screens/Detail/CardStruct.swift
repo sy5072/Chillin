3cbb82fa9c66671ae02514b2b045a10b0eb18023
Nick(Seungyun Kim) <sy5072@naver.com>
2022-05-05 15:53:58 +0900

    [ADD] Card Struct 구현

--- chillin/Screens/Detail/CardStruct.swift
+++ chillin/Screens/Detail/CardStruct.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct TownCard: Identifiable, Hashable {
+    let id: UUID
+    let townName: String
+    var image: [String]?
+    let townGuide: String
+    var isAdded: Bool
+    
+    init(id: UUID = UUID(), townName: String, image: [String]?, townGuide: String, isAdded: Bool){
+        self.id = id
+        self.townName = townName
+        self.image = image
+        self.townGuide = townGuide
+        self.isAdded = isAdded
+    }
+}
+
+extension TownCard {
+    static let sampleData: [TownCard] =
+    [
+        TownCard(townName: "기계면", image: nil, townGuide: "기계면 개쩐당 꼭 가보자 꼭꼭꼭 !!!!! ", isAdded: false)
+    ]
+}
+
